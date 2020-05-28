a4e25e961e81700e21886e4ee3d349adf7f11379
komal909 <65548086+komal909@users.noreply.github.com>
2020-05-28 16:06:44 +0530

    created positive numbers
    It shows all the positive numbers.

---
+++ positive numbers.py
@@ -0,0 +1,20 @@
+Input:
+list1=[12,-7,5,64,-14]
+for i in list1:
+    if i>=0:
+        print(i)
+Output:
+12
+5
+64
+
+Input:
+list1=[12,14,-95,3]
+for i in list1:
+    if i>=0:
+        print(i)
+output:
+12
+14
+3
+
